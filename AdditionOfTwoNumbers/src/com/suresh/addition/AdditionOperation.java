fb6aaecdd659efed9ce374ce6478245835c5e87a
sureshKarri31 <suresh20000531@gmail.com>
2023-05-31 14:58:55 +0530

    workspace is added

---
+++ AdditionOfTwoNumbers/src/com/suresh/addition/AdditionOperation.java
@@ -0,0 +1,13 @@
+package com.suresh.addition;
+
+public class AdditionOperation {
+
+	public static void main(String[] args) {
+		int num1 =3;
+		int num2=4;
+		int result=num1+num2;
+		System.out.println("the sum of two numbers "+result);
+
+	}
+
+}
