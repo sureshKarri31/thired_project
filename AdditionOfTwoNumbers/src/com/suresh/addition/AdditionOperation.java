2534520f564b2109350ffdd7a078366d7165e342
LokeshKanakala007 <lokeshkumarkanakala007@gmail.com>
2023-05-31 18:13:51 +0530

    addition changed

--- AdditionOfTwoNumbers/src/com/suresh/addition/AdditionOperation.java
+++ AdditionOfTwoNumbers/src/com/suresh/addition/AdditionOperation.java
@@ -5,7 +5,8 @@ public class AdditionOperation {
 	public static void main(String[] args) {
 		int num1 =3;
 		int num2=4;
-		int result=num1+num2;
+		int num3 = 10;
+		int result=num1+num2+num3;
 		System.out.println("the sum of two numbers "+result);
 
 	}
